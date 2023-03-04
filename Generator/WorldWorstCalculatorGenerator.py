da5143c97d7c85fcca63ffa3b4b01f6fe07f48f6
GabrielVitorGL <gabrielvitorcom@hotmail.com>
2023-03-03 23:30:40 -0300

    adding Generator folder link

--- Generator/WorldWorstCalculatorGenerator.py
+++ Generator/WorldWorstCalculatorGenerator.py
@@ -1,32 +1,32 @@
-with open('../WorldWorstCalculator.py', 'w') as arquivo:
-    arquivo.write('firstNumber = int(input("Enter the first number: "))\n')
-    arquivo.write('operator = input("Enter the operator (+, -, /, *): ")\n')
-    arquivo.write('secondNumber = int(input("Enter the second number: "))\n')
-    arquivo.write('result = None\n')
+with open('../WorldWorstCalculator.py', 'w') as file:
+    file.write('firstNumber = int(input("Enter the first number: "))\n')
+    file.write('operator = input("Enter the operator (+, -, /, *): ")\n')
+    file.write('secondNumber = int(input("Enter the second number: "))\n')
+    file.write('result = None\n')
 
 
-    arquivo.write('\n# Addition\n\n')
+    file.write('\n# Addition\n\n')
     for i in range(101):
         for j in range(101):
-            arquivo.write("result = " + str(i+j) + " if firstNumber == " + str(i) + " and operator == '+' and secondNumber == " + str(j) + " else result\n")
+            file.write("result = " + str(i+j) + " if firstNumber == " + str(i) + " and operator == '+' and secondNumber == " + str(j) + " else result\n")
 
-    arquivo.write('\n# Subtraction\n\n')
+    file.write('\n# Subtraction\n\n')
     for i in range(101):
         for j in range(101):
-            arquivo.write("result = " + str(i-j) + " if firstNumber == " + str(i) + " and operator == '-' and secondNumber == " + str(j) + " else result\n")
+            file.write("result = " + str(i-j) + " if firstNumber == " + str(i) + " and operator == '-' and secondNumber == " + str(j) + " else result\n")
 
-    arquivo.write('\n# Division\n\n')
+    file.write('\n# Division\n\n')
     for i in range(1, 101):
         for j in range(1, 101):
-            arquivo.write("result = " + str(i/j) + " if firstNumber == " + str(i) + " and operator == '/' and secondNumber == " + str(j) + " else result\n")
+            file.write("result = " + str(i/j) + " if firstNumber == " + str(i) + " and operator == '/' and secondNumber == " + str(j) + " else result\n")
 
-    arquivo.write('\n# Multiplication\n\n')
+    file.write('\n# Multiplication\n\n')
     for i in range(101):
         for j in range(101):
-            arquivo.write("result = " + str(i*j) + " if firstNumber == " + str(i) + " and operator == '*' and secondNumber == " + str(j) + " else result\n")
+            file.write("result = " + str(i*j) + " if firstNumber == " + str(i) + " and operator == '*' and secondNumber == " + str(j) + " else result\n")
 
 
-    arquivo.write('\nif result is not None:\n')
-    arquivo.write('    print("The result is:", result)\n')
-    arquivo.write('else:\n')
-    arquivo.write('    print("The requested operation is invalid.")\n')
+    file.write('\nif result is not None:\n')
+    file.write('    print("The result is:", result)\n')
+    file.write('else:\n')
+    file.write('    print("The requested operation is invalid.")\n')
